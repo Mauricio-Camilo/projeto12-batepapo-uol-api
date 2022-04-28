28647b44dd22915ba5f92b1990fe690d9e4088e2
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-04-28 18:52:06 -0500

    post/participants validaçao de nome vazio feita

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,56 @@
-import express from "express";
+import express, { json } from "express";
+import { MongoClient } from "mongodb";
 import chalk from "chalk";
 import cors from "cors";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(cors());
+app.use(json());
 
-app.get("/", (req,res) => {
-    res.send("OK");
-})
+const participantes = [];
+
+const mensagens = [];
+
+let database = null;
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+const promise = mongoClient.connect();
+    promise.then(() => {
+        database = mongoClient.db("test");
+        console.log(chalk.bold.green("Banco de dados conectado, show!!"))
+    })
+    promise.catch(e => console.log(chalk.bold.red("Deu ruim"), e));
+
+app.post("/participants", (req, res) => {
+    let { name } = req.body;
+    console.log(name);
+    if (!name) {
+        console.log(chalk.bold.red("Nome não enviado"));
+        res.status(422).send("Todos os campos são obrigatórios");
+        return;
+    }
+    const login = {
+        name: name,
+        lastStatus: Date.now()
+    }
+    res.sendStatus(201);
+    // const promise2 = database.collection("usuarios").insertOne(login);
+    // promise2.then(confirmação => {
+    //     console.log("deu bom");
+    //     res.sendStatus(201);
+    // })
+    // promise2.catch(e => {console.log(chalk.bold.red("Deu ruim para fazer login"), e);
+    // res.status(500).send("Não rolou a inserção no db")})
+});
 
 app.listen(5000, () => {
     console.log(chalk.bold.blue("Servidor vivo na porta 5000"));
 })
 
-// Teste de enviar dados para o github
+
+// Gerar horário atual
+// const data = dayjs().format('hh:mm:ss') 
+// console.log(data);
+
+// const agora = Date.now();
+// console.log(agora);
