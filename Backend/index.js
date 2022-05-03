19d4818845fd298b03cf644b7e9d53ab13707426
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-05-03 09:50:42 -0500

    Projeto finalizado

--- Backend/index.js
+++ Backend/index.js
@@ -8,8 +8,6 @@ import dayjs from "dayjs";
 
 dotenv.config();
 
-console.log(process.env.MONGO_URL)  // TIRAR
-
 const app = express();
 app.use(cors());
 app.use(json());
@@ -27,26 +25,15 @@ const messageSchema = Joi.object({
 let database = null;
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 
-// TIRAR
-
-// const promise = mongoClient.connect();
-// promise.then(() => {
-//     database = mongoClient.db("test");
-//     console.log(chalk.bold.green("Banco de dados conectado, show!!"))
-// })
-// promise.catch(e => console.log(chalk.bold.red("Deu ruim"), e));
-
 app.post("/participants", async (req, res) => {
     let { name } = req.body;
-    console.log(chalk.red.bold(name));
     const validação = userSchema.validate(name);
     if (validação.error) {
         res.status(422).send("Todos os campos são obrigatórios");
         return;
     }
-    // SIMPLIFICAR
     const login = {
-        name: name,
+        name,
         lastStatus: Date.now()
     }
     const usuario = {
@@ -60,27 +47,23 @@ app.post("/participants", async (req, res) => {
         await mongoClient.connect();
         database = mongoClient.db("test");
         console.log(chalk.bold.green("Banco de dados conectado, show!!"))
-        const verificaNome = await database.collection("participantesTeste").findOne({ name: name }) // RESUMIR
-        console.log("teste: ", verificaNome);
+        const verificaNome = await database.collection("participantes").findOne({ name: name }) // RESUMIR
 
-        // VERIFICAR QUANDO POSSÍVEL
         if (verificaNome) {
             res.status(409).send("Nome já existente");
             mongoClient.close();
             return;
         }
 
-        // trocar as coleções depois para participantes e mensagens
-
-        await database.collection("participantesTeste").insertOne(login);
+        await database.collection("participantes").insertOne(login);
         console.log(chalk.bold.green("Login salvo no banco"));
 
-        await database.collection("mensagensTeste").insertOne(usuario);
+        await database.collection("mensagens").insertOne(usuario);
         console.log(chalk.bold.green("Usuario salvo no banco"));
 
         mostrarMensagens();
         mostrarParticipantes();
-        setInterval(() => procurarUsuarioInativo(), 5000); // TROCAR PARA 15000
+        setInterval(() => procurarUsuarioInativo(), 15000);
     }
     catch (error) {
         console.error(error);
@@ -96,14 +79,13 @@ function mostrarMensagens() {
         const { limit } = req.query;
         let mensagens = 0;
         try {
-            const participantes = await database.collection("mensagensTeste").find().toArray();
+            const participantes = await database.collection("mensagens").find().toArray();
             const mensagensFiltradas = participantes.filter(participante => {
                 const usuario = participante.from === nome || participante.to === nome;
                 const mensagemPrivada = usuario && participante.type === 'private_message';
                 const mensagemPublica = participante.type === 'status' || participante.type === 'message';
                 return mensagemPrivada || mensagemPublica;
             })
-            console.log(chalk.bold.green("Mensagens obtidas do servidor"));
             if (limit !== "") mensagens = mensagensFiltradas.reverse().slice(0, limit);
             else mensagens = mensagensFiltradas;
             res.send(mensagens.reverse());
@@ -120,8 +102,7 @@ function mostrarParticipantes() {
     app.get("/participants", async (req, res) => {
         const { user: nome } = req.headers;
         try {
-            const participantes = await database.collection("participantesTeste").find().toArray();
-            console.log(chalk.bold.green("Participantes obtidos do servidor"));
+            const participantes = await database.collection("participantes").find().toArray();
             res.send(participantes);
         }
         catch (error) {
@@ -133,8 +114,7 @@ function mostrarParticipantes() {
 
 async function procurarUsuarioInativo() {
     try {
-        const participantes = await database.collection("participantesTeste").find().toArray();
-        console.log(chalk.bold.red(participantes));
+        const participantes = await database.collection("participantes").find().toArray();
         if (participantes === "") {
             mongoClient.close();
             console.log(chalk.bold.blue("Servidor desconectado"));
@@ -152,47 +132,43 @@ async function procurarUsuarioInativo() {
 }
 
 async function removerUsuarioInativo(participantesInativos) {
-    console.log("entrou na função")
-    // try {
-    //     // Testar depois sem esse await
-    //     await participantesInativos.forEach(participante => {
-    //         const mensagem = {
-    //             from: participante.name,
-    //             to: "Todos",
-    //             text: "sai da sala...",
-    //             type: "status",
-    //             time: dayjs().format('hh:mm:ss')
-    //         }
-    //         database.collection("participantesTeste").deleteOne({ _id: new ObjectId(participante._id) })
-    //         database.collection("mensagensTeste").insertOne(mensagem);
-    //     })
-    // }
-    // catch (error) {
-    //     console.error(error);
-    //     res.status(500).send(chalk.red.bold("Falha na obtenção dos participantes"))
-    // }
+    try {
+        await participantesInativos.forEach(participante => {
+            const mensagem = {
+                from: participante.name,
+                to: "Todos",
+                text: "sai da sala...",
+                type: "status",
+                time: dayjs().format('hh:mm:ss')
+            }
+            database.collection("participantes").deleteOne({ _id: new ObjectId(participante._id) })
+            database.collection("mensagens").insertOne(mensagem);
+        })
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send(chalk.red.bold("Falha na obtenção dos participantes"))
+    }
 }
 
 app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const { user: nome } = req.headers;
-    // SIMPLIFICAR O CÓDIGO
     const mensagem = {
         from: nome,
-        to: to,
-        text: text,
-        type: type,
+        to,
+        text,
+        type,
         time: dayjs().format('hh:mm:ss')
     }
 
     const validação = messageSchema.validate(mensagem);
-    console.log(validação);
     if (validação.error) {
-        res.status(422).send("Deu ruim pra enviar a mensagem");
+        res.sendStatus(422);
         return;
     }
     try {
-        await database.collection("mensagensTeste").insertOne(mensagem);
+        await database.collection("mensagens").insertOne(mensagem);
         console.log(chalk.bold.green("Mensagem salva no banco"));
         res.sendStatus(201);
     }
@@ -202,32 +178,12 @@ app.post("/messages", async (req, res) => {
     }
 })
 
-app.delete("/messages/:id", async (req, res) => {
-    const { id } = req.params;
-    const { user: nome } = req.headers;
-    try {
-        const procuraMensagem = await database.collection("mensagensTeste").findOne({ _id: new ObjectId(id) })
-        if (!procuraMensagem) {
-            res.status(404).send("Mensagem não encontrada");
-            return;
-        }
-        else {
-            await database.collection("mensagensTeste").deleteOne({ _id: new ObjectId(id) })
-            res.sendStatus(200);
-        }
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).send(chalk.red.bold("Falha para deletar mensagem"))
-    }
-})
-
 app.post("/status", async (req, res) => {
     const { user: nome } = req.headers;
-    const validação = await database.collection("participantesTeste").findOne({ name: nome })
+    const validação = await database.collection("participantes").findOne({ name: nome })
     if (validação) {
         console.log(chalk.bold.green("Atualização do status feito com sucesso"));
-        await database.collection("participantesTeste").
+        await database.collection("participantes").
             updateOne({ name: nome }, { $set: { lastStatus: Date.now() } });
         res.sendStatus(200);
         return;
@@ -236,6 +192,26 @@ app.post("/status", async (req, res) => {
         console.log(chalk.bold.red("Não deu pra atualizar o status"));
         res.sendStatus(404);
         return;
+    }
+})
+
+app.delete("/messages/:id", async (req, res) => {
+    const { id } = req.params;
+    const { user: nome } = req.headers;
+    try {
+        const procuraMensagem = await database.collection("mensagens").findOne({ _id: new ObjectId(id) })
+        if (!procuraMensagem) {
+            res.sendStatus(404);
+            return;
+        }
+        else {
+            await database.collection("mensagens").deleteOne({ _id: new ObjectId(id) })
+            res.sendStatus(200);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send(chalk.red.bold("Falha para deletar mensagem"))
     }
 })
 
